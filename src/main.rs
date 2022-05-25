3c074c670cd39ae7e487787301eba578eff1b852
Aditya Wasan <adityawasan55@gmail.com>
2022-05-25 13:56:22 +0530

    feat: use `env::var` instead of dotenv macro
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@
 mod url_shortener;
 mod routes;
 mod models;
-mod schema
+mod schema;
 mod controllers;
 
 #[macro_use]
 extern crate rocket;
-extern crate dotenv_codegen;
+extern crate dotenv;
 
-use dotenv_codegen::dotenv;
+use dotenv::dotenv;
+use std::env;
 
 fn main() {
-    let _base_url = dotenv!("SMOL_BASE_URL").to_string();
+    dotenv().ok();
+
+    let _base_url = env::var("SMOL_BASE_URL").unwrap_or_default();
 
     let mut rocket = rocket::ignite();
     rocket = routes::mount_routes(rocket);
