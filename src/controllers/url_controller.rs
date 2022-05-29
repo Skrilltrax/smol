f5c1332034d30f466aa390e7cf9f2560dee8ba41
Aditya Wasan <adityawasan55@gmail.com>
2022-05-29 14:24:52 +0530

    feat: wire up dao and shortener
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- src/controllers/url_controller.rs
+++ src/controllers/url_controller.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use sqlx::types::Uuid;
+use crate::models::url::Url;
 use crate::url_shortener::UrlShortener;
 use crate::UrlDao;
 
@@ -18,12 +19,18 @@ impl<'a> UrlController<'a> {
         Ok(url_result.long_url)
     }
 
-    pub fn save_url(&self, long_url: String) -> Result<String> {
-        let short_url = self.url_shortener.shorten(long_url);
-        Ok(short_url)
+    pub async fn save_url(&self, long_url: String) -> Result<String> {
+        let short_url = self.url_shortener.generate_short_url();
+        let return_url = short_url.to_owned();
+        let url = Url { id: Default::default(), short_url, long_url };
+        self.url_dao.add_url(url).await?;
+
+        Ok(return_url)
     }
 
-    pub fn get_long_url(&self, short_url: String) -> Result<String> {
-        Ok("".to_string())
+    pub async fn get_long_url(&self, short_url: String) -> Result<String> {
+        let long_url = self.url_dao.find_url(short_url).await?;
+
+        Ok(long_url)
     }
 }
