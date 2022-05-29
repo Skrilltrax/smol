e11f3bb79120df6ac6bc7fe4a40ff7a181821d25
Aditya Wasan <adityawasan55@gmail.com>
2022-05-29 14:24:12 +0530

    feat: add method to generate short_url
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- src/url_shortener/mod.rs
+++ src/url_shortener/mod.rs
@@ -1,12 +1,31 @@
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
 #[derive(Eq, PartialEq, Hash)]
 pub(crate) struct UrlShortener {
     base_url: String,
 }
 
 impl UrlShortener {
+    const SHORT_URL_LENGTH: usize = 8;
+
     pub fn new(base_url: String) -> UrlShortener { UrlShortener { base_url } }
 
-    pub fn shorten(&self, url: String) -> String {
-        url
+    pub fn generate_short_url(&self) -> String {
+        let base_url = self.base_url.to_owned();
+        let short_string = self.get_short_part();
+        let short_url = base_url + short_string.as_str();
+
+        short_url
+    }
+
+    fn get_short_part(&self) -> String {
+        let rand_string: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(UrlShortener::SHORT_URL_LENGTH)
+            .map(char::from)
+            .collect();
+
+        rand_string
     }
 }
